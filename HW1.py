c13986cff1f87fb8638a7217e7303f44181ed818
sebnemdogru <40859916+heymamboy@users.noreply.github.com>
2020-12-22 22:30:49 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,11 @@
+lis=[]
+for i in range(0,5):
+    n=input("Please enter a value: ")
+    lis.append(n)
+
+for j in range(len(lis)):
+    print("value: {} and type of a value: {}".format(lis[j],type(lis[j])))
+    
+   
+    
+    
